3edc710f61135283f796be54779c7f59fe1094f5
bshongwe <dru.da.beat.hustle@gmail.com>
2024-05-20 17:44:15 -0700

    Task 0: ✨ Implement BasicCache class to store and retrieve items with put and get methods

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Module: Task 1 - Basic caching
+"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Represents object that allows storing and
+    retrieves items from dictionary
+    """
+
+    def put(self, key, item):
+        """
+        Adds item in the cache
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves item by key
+        """
+        return self.cache_data.get(key, None)
